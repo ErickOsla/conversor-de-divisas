fab72d4518a5f92a01cff84b2eda3bc6010f229d
Erick <automaster502@gmail.com>
2024-11-22 10:50:00 -0600

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  cantidad: number = 0; // Cantidad ingresada en Quetzales
+  resultado: number = 0; // Resultado de la conversión
 
   constructor() {}
 
+  convertir() {
+    if (this.cantidad !== null) {
+      this.resultado = this.cantidad * 8; // Tasa fija de conversión
+    }
+  }
 }
