e17016e4692badcffaf57e18a44ee28ecf2b956b
Erick <automaster502@gmail.com>
2024-11-21 19:42:58 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Conversor De Divisas',
+  webDir: 'www'
+};
+
+export default config;
